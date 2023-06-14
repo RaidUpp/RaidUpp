1a201653fc82362e22a4f189976cbc5d37cf408a
Daniele Cavalcante <39858223+daneccc@users.noreply.github.com>
2023-06-14 08:20:07 -0300

    chore: start basic setup for tvos

---
+++ tvOS/Views/Menu/GuildMenuView.swift
@@ -0,0 +1,20 @@
+//
+//  GuildMenuView.swift
+//  RaidUpp
+//
+//  Created by Daniele Cavalcante on 13/06/23.
+//
+
+import SwiftUI
+
+struct GuildMenuView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct GuildMenuView_Previews: PreviewProvider {
+    static var previews: some View {
+        GuildMenuView()
+    }
+}
---
+++ tvOS/Views/Mission/MissionDetailsView.swift
@@ -0,0 +1,20 @@
+//
+//  MissionDetailsView.swift
+//  RaidUpp
+//
+//  Created by Daniele Cavalcante on 13/06/23.
+//
+
+import SwiftUI
+
+struct MissionDetailsView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct MissionDetailsView_Previews: PreviewProvider {
+    static var previews: some View {
+        MissionDetailsView()
+    }
+}
