f8d82a716fc220b82171b6bdab02d3a3b168926a
Moyses Azevedo <m.mirazev@gmail.com>
2023-06-14 08:42:59 -0300

    :sparkles: feat: FontSizeEnum

---
+++ RaidUpp/Presenter/Enums/FontSizeEnum.swift
@@ -0,0 +1,15 @@
+//
+//  FontSizeEnum.swift
+//  RaidUpp
+//
+//  Created by Moyses Miranda do Vale Azevedo on 14/06/23.
+//
+
+import Foundation
+
+enum FontSizeEnum: Int {
+    case small = 20
+    case medium = 22
+    case large = 28
+    case xlarge = 34
+}
