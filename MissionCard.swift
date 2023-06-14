66773228a1ea767e82ee53bce14468f420fd443e
Daniele Cavalcante <39858223+daneccc@users.noreply.github.com>
2023-06-14 11:33:31 -0300

    feat: create mission card view

---
+++ MissionCard.swift
@@ -0,0 +1,20 @@
+//
+//  MissionCard.swift
+//  RaidUppTvOS
+//
+//  Created by Daniele Cavalcante on 14/06/23.
+//
+
+import SwiftUI
+
+struct MissionCard: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct MissionCard_Previews: PreviewProvider {
+    static var previews: some View {
+        MissionCard()
+    }
+}
---
+++ RaidUppTvOS/Domain/MissionTypeImage.swift
@@ -0,0 +1,8 @@
+//
+//  MissionTypeImage.swift
+//  RaidUppTvOS
+//
+//  Created by Daniele Cavalcante on 14/06/23.
+//
+
+import Foundation
