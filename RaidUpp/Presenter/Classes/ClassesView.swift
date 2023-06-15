8e5d3e31d6441037b605ecc952d640a14d763f7e
Moyses Azevedo <m.mirazev@gmail.com>
2023-06-15 10:10:52 -0300

    :recycle: refactor: ListView

--- RaidUpp/Presenter/Classes/ClassesView.swift
+++ RaidUpp/Presenter/Classes/ClassesView.swift
@@ -8,26 +8,9 @@
 import SwiftUI
 
 struct ClassesView: View {
-    @State var searchText: String = ""
 
     var body: some View {
-        NavigationStack {
-            List {
-                Text("Moyses")
-            }
-            .navigationTitle("Classes")
-            .searchable(text: $searchText,
-                        placement: .navigationBarDrawer,
-                        prompt: "classes")
-            .toolbar {
-                Button {
-                    addClass()
-                }label: {
-                    Image(systemName: "plus")
-                        .font(.system(size: CGFloat(FontSizeEnum.small.rawValue)))
-                }
-            }
-        }
+       ListView(title: "Class", addAction: {})
     }
 }
 
