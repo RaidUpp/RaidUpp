164f4846c2aef156063dd77f18e88930188604e6
Moyses Azevedo <m.mirazev@gmail.com>
2023-06-15 11:54:27 -0300

    :sparkles: feat: add StudentsView

---
+++ RaidUpp/Presenter/Guilds/GuildsView.swift
@@ -0,0 +1,20 @@
+//
+//  GuildsView.swift
+//  RaidUpp
+//
+//  Created by Moyses Miranda do Vale Azevedo on 15/06/23.
+//
+
+import SwiftUI
+
+struct GuildsView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct GuildsView_Previews: PreviewProvider {
+    static var previews: some View {
+        GuildsView()
+    }
+}
---
+++ RaidUpp/Presenter/Students/StudentsView.swift
@@ -0,0 +1,27 @@
+//
+//  StudentsView.swift
+//  RaidUpp
+//
+//  Created by Moyses Miranda do Vale Azevedo on 15/06/23.
+//
+
+import SwiftUI
+
+struct StudentsView: View {
+    @State var isShowForms: Bool = false
+
+    var body: some View {
+        ListView(title: "Students", addAction: {
+            isShowForms.toggle()
+        })
+        .sheet(isPresented: $isShowForms) {
+            GlobalForms(title: "Student") {
+            }
+        }
+    }}
+
+struct StudentsView_Previews: PreviewProvider {
+    static var previews: some View {
+        StudentsView()
+    }
+}
