b35acea8a8a23fb719b301af1a9558492abd357b
Daniele Cavalcante <39858223+daneccc@users.noreply.github.com>
2023-06-16 11:09:24 -0300

    feat: guild menu view created

---
+++ RaidUppTvOS/Utils/GuildDetails.swift
@@ -0,0 +1,48 @@
+//
+//  GuildDetails.swift
+//  RaidUppTvOS
+//
+//  Created by Daniele Cavalcante on 16/06/23.
+//
+
+import SwiftUI
+
+struct GuildDetails: View {
+    let guild: String
+    let guildName: String
+    let mentor: String
+    let guildDescription: String
+    
+    init(guild: String, guildName: String, mentor: String, guildDescription: String) {
+        self.guild = guild
+        self.guildName = guildName
+        self.mentor = mentor
+        self.guildDescription = guildDescription
+    }
+    
+    var body: some View {
+        VStack(alignment: .leading) {
+            Image("design")
+                .resizable()
+                .frame(width: 794, height: 530)
+                .scaledToFill()
+            Text(self.guildName)
+                .font(.headline)
+                .foregroundColor(.black)
+            Text(self.mentor)
+                .font(.callout)
+                .foregroundColor(.gray)
+            Text(self.guildDescription)
+                .font(.body)
+                .foregroundColor(.gray)
+        }
+        .frame(width: 790, height: 742)
+        .padding(.bottom, -78)
+    }
+}
+
+struct GuildDetails_Previews: PreviewProvider {
+    static var previews: some View {
+        GuildDetails(guild: "Design", guildName: "Nome personalizado", mentor: "Carolina Barbalho", guildDescription: "A guilda valoriza a experimentação e encoraja seus membros a explorarem novas abordagens, técnicas e soluções inovadoras no campo do design. O objetivo é impulsionar a criatividade e estimular o pensamento fora da caixa, resultando em projetos originais e impactantes.")
+    }
+}
