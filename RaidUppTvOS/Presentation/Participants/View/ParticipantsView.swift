211aba18a0fe62fee5abdd4bf89980e1a03d50cc
Daniele Cavalcante <39858223+daneccc@users.noreply.github.com>
2023-06-23 11:42:16 -0300

    fix: grid is now focusable

--- RaidUppTvOS/Presentation/Participants/View/ParticipantsView.swift
+++ RaidUppTvOS/Presentation/Participants/View/ParticipantsView.swift
@@ -8,49 +8,77 @@
 import SwiftUI
 
 struct ParticipantsView: View {
+    
+    let participants: [[String]] = [
+        ["participant-00", "participant-00", "participant-00", "participant-00", "participant-00"],
+        ["participant-01", "participant-01", "participant-01", "participant-01", "participant-01"],
+        ["participant-02", "participant-02", "participant-02", "participant-02", "participant-02"]
+    ]
+    
     @State private var selectedIndex: Int = 0
     @State private var isFocused: Bool = false
-    
+        
     @FocusState var selected: Int?
     
     var body: some View {
         HStack {
             VStack(alignment: .leading, spacing: 32) {
                 Button(action: {
-                    //self.selectedIndex = 0
+                    self.selectedIndex = 0
                 }) {
                     Text("Code")
                 }
-                .buttonStyle(.plain)
                 .focused($selected, equals: 0)
                 
                 Button(action: {
-                    //self.selectedIndex = 1
+                    self.selectedIndex = 1
                 }) {
                     Text("Design")
                 }
-                .buttonStyle(.plain)
                 .focused($selected, equals: 1)
                 
                 Button(action: {
-                    //self.selectedIndex = 2
+                    self.selectedIndex = 2
                 }) {
                     Text("Management")
                 }
-                .buttonStyle(.plain)
                 .focused($selected, equals: 2)
             }
             .frame(width: 571, height: 278)
-
-            VStack(alignment: .leading) {
-                if let selected {
-                    Text("Conteúdo \(selected + 1)")
-                } else {
-                    Text("Passa o controle por cima de um botão")
+            .font(.body)
+            .buttonStyle(.plain)
+            .onChange(of: selected) { newValue in
+                if let newValue {
+                    selectedIndex = newValue
+                    print(selectedIndex)
                 }
             }
-
+            
+            ScrollView {
+                LazyVGrid(
+                    columns: [
+                        GridItem(.flexible()),
+                        GridItem(.flexible()),
+                        GridItem(.flexible()),
+                        GridItem(.flexible())
+                    ],
+                    spacing: 50
+                ) {
+                    ForEach(participants[selectedIndex].indices, id: \.self) { index in
+                        Button(action: {}) {
+                            ParticipantButton(
+                                imageName: participants[selectedIndex][index],
+                                name: "daniele"
+                            )
+                        }.buttonStyle(.plain)
+                    }
+                }
+            }
+            
             Spacer()
+        }
+        .background {
+            Image("background")
         }
         .padding()
         .frame(maxWidth: .infinity, maxHeight: .infinity)
